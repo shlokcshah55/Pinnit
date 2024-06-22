04af93b595779205d64a689f6207e13c2e07a3fd
shlokcshah55 <shahshlok035@gmail.com>
2024-06-22 14:46:24 +0100

    [ss] Cloned files

---
+++ lib/pages/google_map_page.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+// class GoogleMapPage extends StatefulWidget {
+//   const GoogleMapPage({super.key});
+
+//   @override
+//   State<GoogleMapPage> createState() => _GoogleMapPageState();
+
+// }
+
+// class _GoogleMapPageState extends State<GoogleMapPage> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//     body: GoogleMap(),
+//   );
+// }
